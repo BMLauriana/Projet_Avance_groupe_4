8726fe416014cc8f61b6f04708e1eaaf46f003b7
Lauriana <lauriana.be-manantsoa@univ-tlse3.fr>
2025-10-14 11:28:55 +0200

    definition des structures

--- lecture_donnees.h
+++ lecture_donnees.h
@@ -1,5 +1,22 @@
 #ifndef __LECTURE_DONNEES_H__
 #define __LECTURE_DONNEES_H__
 
+/*************************
+ * Structure d'un noeud
+ *************************/
+typedef struct noeud_s{
+    int num; /*numero du noeud*/
+    float x; /*permiere coordonnee du noeud*/
+    float y; /*deuxiemene coordonnee du neoud*/
+} noeud_t;
+
+/***************************
+ * Structure d'une tournee
+ ***************************/
+
+typedef struct tournee_s{
+    float longueur; /*longueur de la tournee*/
+    int *parcours; /*tableau de noeuds dans l'ordre de la tournee*/
+} tournee_t;
 
 #endif
