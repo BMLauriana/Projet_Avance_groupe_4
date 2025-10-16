5d6f7719b775798e20fe79e4465f3ad20388182e
Melanie <r.tiamela@gmail.com>
2025-10-16 22:23:55 +0200

    typedef instance + fichier python

--- C/lecture_donnees.h
+++ C/lecture_donnees.h
@@ -21,4 +21,22 @@ typedef struct tournee_s{
     int *parcours; /*tableau de noeuds dans l'ordre de la tournee*/
 } tournee_t;
 
+//elles sont écrites au format texte dans les fichiers 
+/***************************
+ * Structure d'une instance (jeux de donnée)
+ ***************************/
+typedef struct instance_s{
+    char nom[64];  // nom de l'instance
+    char type_distance[16]; //EUc_2D, GEO, ATT
+    int dimension; //nombre de ville (points)
+    noeud_t *noeuds; // tableau contenant les coordonnées de chaque villes
+}instance_t;
+
+
+
+//matrice de dsitances 
+
+float **creer_matrice(instance_t inst, float(*f_distance)(noeud_t, noeud_t));
+float obtenir_distance(float **matrice , int i , int j);
+void liberer_matrice(float **matrice, int n);
 #endif
