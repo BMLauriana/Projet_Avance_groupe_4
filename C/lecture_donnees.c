6b9a9e03760a2d22463dc300dddffcfc6719e358
Melanie <r.tiamela@gmail.com>
2025-10-19 22:36:35 +0200

    definition n des struct dans le.c

--- C/lecture_donnees.c
+++ C/lecture_donnees.c
@@ -4,6 +4,41 @@
 #include <string.h>
 #include "lecture_donnees.h"
 
+#define PI 3.141592
+#define MAX_TAB_NOM 64
+#define MAX_TAB_DISTANCE 16
+
+/*************************
+ * Structure d'un noeud
+ *************************/
+
+struct noeud_s{
+    int num; /*numero du noeud*/
+    float x; /*permiere coordonnee du noeud*/
+    float y; /*deuxiemene coordonnee du neoud*/
+};
+
+/***************************
+ * Structure d'une tournee
+ ***************************/
+
+struct tournee_s{
+    float longueur; /*longueur de la tournee*/
+    noeud_t *parcours; /*tableau de noeuds dans l'ordre de la tournee*/
+};
+
+//elles sont écrites au format texte dans les fichiers 
+/***************************
+ * Structure d'une instance (jeux de donnée)
+ ***************************/
+typedef struct instance_s{
+    char nom[MAX_TAB_NOM];  // nom de l'instance
+    char type_distance[MAX_TAB_DISTANCE]; //EUc_2D, GEO, ATT
+    int dimension; //nombre de ville (points)
+    noeud_t *noeuds; // tableau contenant les coordonnées de chaque villes
+}instance_t;
+
+
 //chemin is the file path 
 int lire_tsplib(const char *chemin, instance_t *inst)
 {
--- C/lecture_donnees.h
+++ C/lecture_donnees.h
@@ -1,38 +1,9 @@
 #ifndef __LECTURE_DONNEES_H__
 #define __LECTURE_DONNEES_H__
 
-#define PI 3.141592
-#define MAX_TAB_NOM 64
-#define MAX_TAB_DISTANCE 16
-
-/*************************
- * Structure d'un noeud
- *************************/
-typedef struct noeud_s{
-    int num; /*numero du noeud*/
-    float x; /*permiere coordonnee du noeud*/
-    float y; /*deuxiemene coordonnee du neoud*/
-} noeud_t;
-
-/***************************
- * Structure d'une tournee
- ***************************/
-
-typedef struct tournee_s{
-    float longueur; /*longueur de la tournee*/
-    noeud_t *parcours; /*tableau de noeuds dans l'ordre de la tournee*/
-} tournee_t;
-
-//elles sont écrites au format texte dans les fichiers 
-/***************************
- * Structure d'une instance (jeux de donnée)
- ***************************/
-typedef struct instance_s{
-    char nom[MAX_TAB_NOM];  // nom de l'instance
-    char type_distance[MAX_TAB_DISTANCE]; //EUc_2D, GEO, ATT
-    int dimension; //nombre de ville (points)
-    noeud_t *noeuds; // tableau contenant les coordonnées de chaque villes
-}instance_t;
+typedef struct noeud_s noeud_t;
+typedef struct tournee_s tournee_t;
+typedef struct instance_s instance_t;
 
 //lecture de fichier
 instance_t lire_tsplib(const char *chemin);
@@ -49,10 +20,10 @@ float longueur_tournee(instance_t instance,tournee_t tour, int(*f_distance)(noeu
 /*fonction de selection de la fonction de distance en fonction de l'instance*/
 int(*f_distance)(noeud_t,noeud_t) choix_distance(instance_t inst);
 
+
 /*******************************
 matrice inférieure de distance 
 *******************************/ 
-
 int **creer_matrice(instance_t inst, int(*f_distance)(noeud_t, noeud_t));
 int recuperer_distance(int **matrice , int i , int j);
 /*calcule la longueur de la tournee canonique a l'aide de la matrice*/
