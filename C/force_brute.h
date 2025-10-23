5a2fdddad20fdfef8191ee8dd3b732cec397534b
Melanie <r.tiamela@gmail.com>
2025-10-24 00:28:57 +0200

    déclaration des fonctions dans le.h

--- C/force_brute.h
+++ C/force_brute.h
@@ -1,4 +1,8 @@
 #ifndef __FORCE_BRUTE_H__
 #define __FORCE_BRUTE_H_
+#include "lecture_donnees.h"
+
+int next_permutation(int *tab, int n);
+tournee_t force_brute (instance_t *inst, int **matrice);
 
 #endif
