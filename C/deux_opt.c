0af94490af593313d0886b947c1202f55bfcba9d
Lauriana <lauriana.be-manantsoa@univ-tlse3.fr>
2025-11-11 16:41:03 +0100

    début 2-opt

--- C/deux_opt.c
+++ C/deux_opt.c
@@ -1,1 +1,29 @@
 #include "heuristiques.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+tournee_t * deux_opt(tournee_t * tournee, int ** matrice, int n){
+    tournee_t * res = malloc(sizeof(tournee_t));
+    res->parcours =malloc(n*sizeof(noeud_t));
+
+    if (!tournee || !res->parcours){
+        fprintf(stderr, "Erreur d’allocation mémoire\n");
+        exit(EXIT_FAILURE);
+    }
+
+    res->longueur = tournee->longueur;
+
+    for (int i = 0; i < n; i++) {
+        res->parcours[i] = tournee->parcours[i];
+    }
+
+    float nouvelle_longueur;
+
+    for(int i=0; i<n-2;i++){
+        for(int j=0; i<n-1;j++){
+            /*etape */
+        }
+    }
+
+    return res;
+}
--- C/heuristiques.h
+++ C/heuristiques.h
@@ -3,7 +3,6 @@
 #include "lecture_donnees.h"
 
 tournee_t * plus_proche_voisin(instance_t * inst, int** matrice);
-void permuter(int *a, int *b);
 tournee_t marche_aleatoire_matrice(instance_t *inst, int **matrice);
 
 #endif
