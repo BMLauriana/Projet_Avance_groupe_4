782e694cd5113d49d119ad13f1f3ad8c6886a07d
Melanie <r.tiamela@gmail.com>
2025-11-13 14:07:12 +0100

    modificationn de force_brute.h

--- C/force_brute.h
+++ C/force_brute.h
@@ -3,7 +3,7 @@
 #include "lecture_donnees.h"
 
 int next_permutation(int *tab, int n);
-tournee_t force_brute (instance_t *inst, int **matrice);
-tournee_t force_brute2(instance_t *inst, distance_f f_distance) ;
+tournee_t *force_brute (instance_t *inst, int **matrice);
+tournee_t *force_brute2(instance_t *inst, distance_f f_distance) ;
 
 #endif
