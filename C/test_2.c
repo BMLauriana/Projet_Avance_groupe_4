45f9e150ae7e7ea7ca5bc5798cfa201f91a148c9
Melanie <r.tiamela@gmail.com>
2025-11-13 15:16:33 +0100

    test de random walk

--- C/test_2.c
+++ C/test_2.c
@@ -33,13 +33,22 @@ int main(void){
     assert(tour1->parcours[1].num==2);
     assert(tour1->parcours[2].num==3);
 
-    printf("TEST 2 OK\n");
+    tournee_t * tour2 = marche_aleatoire_matrice(instance, demi_matrice);
+    //assert pour savoir si il y a des doublouns ou pas 
+    int vus[4]={0}; // ndices 1..3 utilisés
+    for (int i = 0; i < instance->dimension; i++) {
+        int num = tour2->parcours[i].num;
+        assert(num >= 1 && num <= 3);   // numéro valide
+        assert(vus[num] == 0);          //elle ne dois pas avoir été vu avant
+        vus[num] = 1; //on marque elle a été vu 
+    }
+    printf("TEST NN ET RW OK\n");
 
     liberer_tournee(&tour1);
+    liberer_tournee(&tour2);
     liberer_matrice(demi_matrice, instance->dimension);
     free(instance->noeuds);
     free(instance);
-    
 
     return 0;
 }
