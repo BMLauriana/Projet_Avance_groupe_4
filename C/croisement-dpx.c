4247afbb14641ad17c0869557c5e511490e542b4
ZahraTavakol <zahra.tavakol@univ-tlse3.fr>
2025-12-05 17:00:48 +0100

    arretes_communs

--- C/croisement-dpx.c
+++ C/croisement-dpx.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "genetique.h"
+static int aretes_communes(int position, tournee_t *p1,tournee_t *p2,int dimension)
+{
+    int a = p1->parcours[position].num;
+    int b = p1->parcours[(position + 1) % dimension].num;
+
+    for (int j = 0; j < dimension; j++) {
+        int c = p2->parcours[j].num;
+        int d = p2->parcours[(j + 1) % dimension].num;
+        if ((c == a && d == b) || (c == b && d == a)) {
+            return 1; 
+        }
+    }
+    return 0; 
+}
 
 tournee_t* dpx_crossover(tournee_t* parent1, tournee_t* parent2, int dimension, int** matrice, instance_t* inst) {
     
@@ -14,7 +28,24 @@ tournee_t* dpx_crossover(tournee_t* parent1, tournee_t* parent2, int dimension, 
     //2-Trouver les arêtes communes entre parent1 et parent2
 
     //3-Détruire les arêtes NON communes
-    
+
+    int *frag_id = malloc(dimension * sizeof(int));
+
+    int nb_frag = 0;
+    frag_id[0] = 0;   
+    nb_frag = 1;
+
+    for (int i = 0; i < dimension - 1; i++) {
+        if (aretes_communes(i, parent1, parent2, dimension)) {
+
+            frag_id[i+1] = nb_frag - 1;
+        } else {
+            frag_id[i+1] = nb_frag;
+            nb_frag++;
+        }
+    }
+
+
     //4- Reconnecter les fragments déconnectés
     
     //5-Appliquer 2-opt pour améliorer
@@ -22,6 +53,7 @@ tournee_t* dpx_crossover(tournee_t* parent1, tournee_t* parent2, int dimension, 
     
     free(fille->parcours);
     free(fille);
+    free(frag_id);
     
     return fille_optimisee;
 }
