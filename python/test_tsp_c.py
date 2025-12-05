e06bf9e4a6406d5be23a6200097855fca40eed55
Lauriana <lauriana.be-manantsoa@univ-tlse3.fr>
2025-12-05 21:45:55 +0100

    ajout att48

--- python/test_tsp_c.py
+++ python/test_tsp_c.py
@@ -115,10 +115,10 @@ def tests_instances_list(instances_file,methods):
 # Liste de paramètres.
 # opt = tsplib95.load_solution(filename+'.opt.tour') pour charger un fichier solution dans python
 #filename = "burma14.tsp"
-filename = "../jeu_de_donnees/att10.tsp" #
+filename = "../jeu_de_donnees/att48.tsp" #
 path = "../C/" # adaptez à votre cas
 code = "main" # nom du programe C partie 1
-methods = ["-nn"]#["-ga 30 1000 0.10"]#["ga 30 1000 0.10"]#["-c"],"nn","rw","2optnn","2optrw","ga 10000 0.10 100"]#"bf","gadpx"
+methods = ["-gadpx 30 1000 0.10f"]#["ga 30 1000 0.10"]#["-c"],"nn","rw","2optnn","2optrw","ga 10000 0.10 100"]#"bf","gadpx"
 
 test_instance(filename,methods) # pour un seul appel
 #tests_instances_list("../jeu_de_donnees/instances.txt",methods) # marche si le C marche
